d809c4e415957db5be5b2aecd63958a8223f7fbd
aferreirap <agustinferreira14@gmail.com>
2021-11-10 18:08:15 +0100

    preprocessing

--- preprocessing.py
+++ preprocessing.py
@@ -1,1 +1,36 @@
 import pandas as pd
+import numpy as np
+from PIL import Image
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import os
+import cv2
+
+def processing(img,height=int,width=int):
+    dim = (width,height)
+    size =  cv2.resize(img,dim, interpolation=cv2.INTER_LINEAR)
+    norm = np.zeros((255, 255))
+    norm_img = cv2.normalize(size, norm, 0, 255, cv2.NORM_MINMAX)
+    
+    return  cv2.GaussianBlur(norm_img, (5, 5), 0)
+
+def get_images():
+    df = pd.DataFrame(columns=['App','Image'])
+
+    app_images = [x for x in os.listdir(('./apps_imgs')) if str(x) != '.DS_Store']
+
+    for image in app_images:
+        try: 
+            crude  = np.asarray(Image.open(f'./apps_imgs/{image}')) #transforming image to array
+            processed_img = processing(crude,255,255) #resizing, normalizing and bluring image
+        except IOError:
+            pass
+        image_name = image[:-4]
+        df = df.append({'App' : image_name, 'Image': processed_img} , ignore_index=True)
+    return df
+
+app_data = pd.read_csv('./app_sample.csv')
+
+app_images = get_images()
+img = Image.fromarray(app_images.iloc[1,1])
+img.show()
